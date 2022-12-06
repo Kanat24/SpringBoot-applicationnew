bf16d014c9da4d5fed6c0601468e2f57936fe6e6
Sajer24 <sajer.kanat1992@gmail.com>
2022-12-06 23:54:04 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/SpringBootApplicationnewApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationnewApplication.class, args);
+	}
+
+}
